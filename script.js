62f52790b6a6ab1a380eb2a59ea6bf1f3c234f27
Sekoutouree <edwinsamm@gmail.com>
2022-10-17 07:18:39 +0000

    Updated sript.js

--- script.js
+++ script.js
@@ -17,10 +17,21 @@ document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
 
+  //When there's no input
   if (!guess) {
     document.querySelector('.message').textContent = '⛔ No number selected!';
+
+    //When player wins
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = 'Correct Number!';
+
+    document.querySelector('body').style.backgroundColor = '#60b347';
+
+    document.querySelector('.number').style.width = '30rem';
+
+    document.querySelector('.number').style.color = '#60b347';
+
+    //When guess is too high
   } else if (guess > secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = 'Too high, try again!';
@@ -30,6 +41,8 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.message').textContent = 'You lost!';
       document.querySelector('.score').textContent = 0;
     }
+
+    //When guess is too low
   } else if (guess < secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = 'Too low, try again!';
