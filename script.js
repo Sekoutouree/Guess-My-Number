9d3ee841f25994b53a6e172795895c4f236e0273
Sekoutouree <edwinsamm@gmail.com>
2022-10-17 11:11:25 +0000

    Added highscore functionality sript.js

--- script.js
+++ script.js
@@ -10,6 +10,7 @@
 
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
+let highScore = 0;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
@@ -30,6 +31,12 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('.number').style.width = '30rem';
 
     document.querySelector('.number').style.color = '#60b347';
+
+    //Highscore
+    if (score > highScore) {
+      highScore = score;
+      document.querySelector('.highscore').textContent = highScore;
+    }
 
     //When guess is too high
   } else if (guess > secretNumber) {
